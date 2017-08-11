fe632834b8861bf71204bdc0ce0cc14ff3e051ea
malike <st.malike@gmail.com>
2017-08-11 07:21:35 +0000

    setup... too busy to work on it like I want to :(

--- db/model.go
+++ db/model.go
@@ -1,1 +1,24 @@
 package db
+
+import "time"
+
+type Message struct {
+	Id          string
+	ReferenceId string
+	AlertId     string
+	Content     string
+	Recipient   string
+	ApiResponse MessageResponse
+	DateCreated time.Time
+}
+
+type MessageResponse struct {
+	Response       string
+	TimeOfResponse time.Time
+}
+
+type Template struct {
+	ID             string
+	Content        string
+	TimeOfResponse time.Time
+}
