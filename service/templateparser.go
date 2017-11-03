b2538fc7069b6eb8783f7eceb3c7aa0c032bcaac
malike <st.malike@gmail.com>
2017-11-03 22:45:17 +0000

    comments for template parsing

--- service/templateparser.go
+++ service/templateparser.go
@@ -7,6 +7,7 @@ import (
 	"text/template"
 )
 
+//ParseTemplateForMessage : Parses Template
 func ParseTemplateForMessage(event db.Event, channel string) (string, error) {
 	var parse string
 	temp := util.AppConfiguration.Templates[event.EventType+"_"+channel]
