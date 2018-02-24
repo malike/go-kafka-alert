4f673c179a4990496b04d3616b66e7eb58fe8196
malike <st.malike@gmail.com>
2018-02-24 14:22:02 +0000

    fixed errors after lint fix

--- db/mongodb_test.go
+++ db/mongodb_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 var msg = Message{
-	MessageId:   "1234",
+	MessageID:   "1234",
 	Content:     "Sample Message",
-	AlertId:     "Test1234",
+	AlertID:     "Test1234",
 	Subject:     "Sample Subject",
 	Recipient:   "st.malike@gmail.com",
 	DateCreated: time.Now(),
@@ -18,7 +18,7 @@ var msg = Message{
 
 var messageIds = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 
-var messageResponse = MessageResponse{
+var fakeMessageResponse = MessageResponse{
 	APIStatus:      "DELIVERED:123456",
 	Response:       "SENT",
 	Status:         "SUCCESS",
@@ -31,12 +31,12 @@ func TestIndexFindAndRemoveMessage(t *testing.T) {
 		t.Fatal("Error saving message " + err.Error())
 		t.FailNow()
 	}
-	message, _ := msg.FindMessage(msg.MessageId)
+	message, _ := msg.FindMessage(msg.MessageID)
 	if message.Subject != msg.Subject {
 		t.Fatal("Error finding message ")
 		t.FailNow()
 	}
-	if deleted := msg.RemoveMessage(msg.MessageId); !deleted {
+	if deleted := msg.RemoveMessage(msg.MessageID); !deleted {
 		t.Fatal("Message not removed")
 		t.FailNow()
 	}
@@ -49,13 +49,13 @@ func TestUpdateResponseAndRemove(t *testing.T) {
 		t.FailNow()
 	}
 
-	message, _ := msg.UpdateResponse(msg.MessageId, messageResponse)
-	if message.MessageResponse != messageResponse {
+	message, _ := msg.UpdateResponse(msg.MessageID, fakeMessageResponse)
+	if message.MessageResponse != fakeMessageResponse {
 		t.Fatal(fmt.Printf("Error updating  message response. Expected '%s', Got '%s'",
-			message.MessageResponse.Status, messageResponse.Status))
+			message.MessageResponse.Status, fakeMessageResponse.Status))
 		t.FailNow()
 	}
-	if deleted := msg.RemoveMessage(msg.MessageId); !deleted {
+	if deleted := msg.RemoveMessage(msg.MessageID); !deleted {
 		t.Fatal("Message not removed")
 		t.FailNow()
 	}
@@ -64,7 +64,7 @@ func TestUpdateResponseAndRemove(t *testing.T) {
 func TestFindAllMessagesByReferenceAndRemoveAll(t *testing.T) {
 	RemoveAllMessagesByReference("SaveMultipleTest")
 	for _, it := range messageIds {
-		msg.MessageId = it
+		msg.MessageID = it
 		msg.Reference = "SaveMultipleTest"
 		msg.IndexMessage()
 	}
@@ -79,7 +79,7 @@ func TestFindAllMessagesByReferenceAndRemoveAll(t *testing.T) {
 func TestCountAllMessagesByReference(t *testing.T) {
 	RemoveAllMessagesByReference("CountByReferenceTest")
 	for _, it := range messageIds {
-		msg.MessageId = it
+		msg.MessageID = it
 		msg.Reference = "CountByReferenceTest"
 		msg.IndexMessage()
 	}
@@ -94,7 +94,7 @@ func TestCountAllMessagesByReference(t *testing.T) {
 func BenchmarkMessage_IndexMessage(b *testing.B) {
 	RemoveAllMessagesByReference("BenchMarkTest")
 	for i := 0; i < b.N; i++ {
-		msg.MessageId = strconv.Itoa(i)
+		msg.MessageID = strconv.Itoa(i)
 		msg.Reference = "BenchMarkTest"
 		msg.IndexMessage()
 	}
@@ -105,12 +105,12 @@ func BenchmarkMessage_IndexMessage(b *testing.B) {
 func BenchmarkMessage_IndexAndUpdateMessage(b *testing.B) {
 	RemoveAllMessagesByReference("BenchMarkTestIndexUpdate")
 	for i := 0; i < b.N; i++ {
-		msg.MessageId = strconv.Itoa(i)
+		msg.MessageID = strconv.Itoa(i)
 		msg.Reference = "BenchMarkTestIndexUpdate"
 		msg.IndexMessage()
 
-		messageResponse.Response = strconv.Itoa(i) + "Response"
-		msg.UpdateResponse(msg.MessageId, messageResponse)
+		fakeMessageResponse.Response = strconv.Itoa(i) + "Response"
+		msg.UpdateResponse(msg.MessageID, fakeMessageResponse)
 	}
 	RemoveAllMessagesByReference("BenchMarkTestIndexUpdate")
 }
